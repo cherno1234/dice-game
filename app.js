9f4b8f73b331a219fc4967f8864d71f63d29f7ee
Cherno <rasuka0502@gmail.com>
2020-09-09 01:26:51 +0000

    finsihed

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //Togloomiin bvkh gazart ashigladakh global huwisagchiig end zarlay
+//Tolgoom duussan esekhiig hadgalakh tolowiin huwisagch
+var isNewGame;
+
 var activePlayer, scores, roundScore;
 //Shoonii zurgiig uzuulekh elementiin Dom-s haij olood ene hadgalay
 var diceDom = document.querySelector(".dice");
@@ -9,6 +12,10 @@ initGame();
 
 //togloomiig shineer ehlehed beltgene
 function initGame() {
+  //Togloom ekhellee gedeg tolowt oruulna.
+
+  isNewGame = true;
+
   diceDom.style.display = "none";
   //Toglogchiin eeljiig hadgalakh huwisagch,player 1: 0,player 2:1
   activePlayer = 0;
@@ -39,48 +46,56 @@ function initGame() {
 
 // shoog shidekh event listenner
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  //1-6 dotorkh sanamsargui toog  garagaj awna
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  //Shoonii zurgiig web dee gargaj irne
-  diceDom.style.display = "block";
-  //Buusan sanamsargui toond hargalzakh shoonii zurgiig web deer gargaj irne.
-  diceDom.src = "dice-" + diceNumber + ".png";
+  if (isNewGame) {
+    //1-6 dotorkh sanamsargui toog  garagaj awna
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    //Shoonii zurgiig web dee gargaj irne
+    diceDom.style.display = "block";
+    //Buusan sanamsargui toond hargalzakh shoonii zurgiig web deer gargaj irne.
+    diceDom.src = "dice-" + diceNumber + ".png";
 
-  //Buusan tooog 1-s ylgaatai bol idewehtei toglogchiin eeljiin onoog oorchilnoo
-  if (diceNumber !== 1) {
-    //1-s ylgaatai too buulaa
-    roundScore = roundScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
-  } else {
-    //1 buusan toglogchiig eeljiig ene hesegt solij ogno .
-    //ene toglogchiin eeljindee tsuglasan toog 0 bolgon oo
-    switchToNextPlayer();
+    //Buusan tooog 1-s ylgaatai bol idewehtei toglogchiin eeljiin onoog oorchilnoo
+    if (diceNumber !== 1) {
+      //1-s ylgaatai too buulaa
+      roundScore = roundScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      //1 buusan toglogchiig eeljiig ene hesegt solij ogno .
+      //ene toglogchiin eeljindee tsuglasan toog 0 bolgon oo
+      switchToNextPlayer();
+    }
   }
 });
 
 // Hold button event Listenner
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  //Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer ni nemj ogno .
-  scores[activePlayer] = scores[activePlayer] + roundScore;
+  if (isNewGame) {
+    //Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer ni nemj ogno .
+    scores[activePlayer] = scores[activePlayer] + roundScore;
 
-  //Delgets deerkh onoog oorchilno
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
+    //Delgets deerkh onoog oorchilno
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
 
-  //ug toglogch hojson esekhiig shalgakh 100-s ikh bol hojno
+    //ug toglogch hojson esekhiig shalgakh 100-s ikh bol hojno
 
-  if (scores[activePlayer] >= 10) {
-    //Ylagch gesen textiig nerniih ni orond gargana....
-    document.getElementById("name-" + activePlayer).textContent =
-      "Winner !!!!!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    switchToNextPlayer();
+    if (scores[activePlayer] >= 10) {
+      //Togloomiig duussan tolowt oruulana
+      isNewGame = false;
+      //Ylagch gesen textiig nerniih ni orond gargana....
+      document.getElementById("name-" + activePlayer).textContent =
+        "Winner !!!!!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      switchToNextPlayer();
+    }
   }
 });
 
